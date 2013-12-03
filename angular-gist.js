d4225849893d654fefb07580f8489382a775ad95
Scott Corgan <scottcorgan@gmail.com>
2013-12-03 12:23:24 -0800

    Update element variable name.

--- angular-gist.js
+++ angular-gist.js
@@ -6,14 +6,14 @@ angular.module('gist')
       restrict: 'E',
       replace: true,
       template: '<div></div>',
-      link: function(scope, elm, attrs) {
+      link: function(scope, element, attrs) {
         var gistId = attrs.id;
 
         var iframe = document.createElement('iframe');
         iframe.setAttribute('width', '100%');
         iframe.setAttribute('frameborder', '0');
         iframe.id = "gist-" + gistId;
-        elm[0].appendChild(iframe);
+        element[0].appendChild(iframe);
 
         var iframeHtml = '<html><head><base target="_parent"><style>table{font-size:12px;}</style></head><body onload="parent.document.getElementById(\'' + iframe.id + '\').style.height=document.body.scrollHeight + \'px\'"><scr' + 'ipt type="text/javascript" src="https://gist.github.com/' + gistId + '.js"></sc'+'ript></body></html>';
 
